c2d316ca98aa64b6613daf16322abbf7244b95ec
mic-00 <michele.masetto@gmail.com>
2023-03-31 14:45:13 +0200

    Luigi insult

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 import MessagesList from './MessagesList';
 import TextInput from './TextInput';
 import './App.css';
@@ -9,6 +10,22 @@ function App() {
     from: 'You',
     content: 'Hello World!'
   }]);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      const name = 'Mario';
+      const reason = 'I like to insult you';
+      axios
+          .get(`https://api.humorapi.com/insult?api-key=${process.env.REACT_APP_API_KEY}&name=${name}&reason=${reason}`)
+          .then(res => {
+            setMessages(messages => [ ...messages, {
+              from: 'Luigi',
+              content: res.data.text
+            } ]);
+          });
+    }, 5000);
+    return () => clearInterval(id);
+  }, []);
 
   const handleSubmit = (event) => {
     setMessages([ ...messages, {
